d3160a434c4e1102a2bf057239dfd59e4d225da5
Tyler Gillies <tjgillies@gmail.com>
2016-11-05 15:02:05 -0700

    Update authorization function

--- lib/webmention/authorize.ex
+++ lib/webmention/authorize.ex
@@ -6,26 +6,28 @@ defmodule Webmention.Authorize do
   This plug is for your webmention controller, use it to verify and create webmentions
   """
 
-  def init(token) do
-    token
+  def init(token_func) do
+    token_func
   end
 
-  def call(conn, token) do
+  def call(conn, token_func) do
     auth_header =
       conn.req_headers
       |> Enum.into(%{})
       |> Map.get("authorization")
     auth_header = auth_header || ""
-    captures = Regex.named_captures(~r/(?<token>#{token})/, auth_header)
-    case captures do
-      nil ->
-        Logger.debug "No Token"
+    Logger.debug inspect auth_header
+    captures = Regex.named_captures(~r/Bearer (?<token>.*)/, auth_header) || %{}
+    Logger.debug inspect captures
+    token = Map.get(captures, "token")
+    authorized? = token_func.(conn, token)
+    case authorized? do
+      false ->
         conn
         |> put_resp_header("WWW-Authenticate", "Bearer")
         |> put_resp_header("Link", "<http://localhost:4000/indie/token>; rel=\"token_endpoint\"")
         |> send_resp(401, "Token required")
-      val ->
-        Logger.debug inspect val
+      true ->
         conn
     end
   end
